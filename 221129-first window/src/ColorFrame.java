c5d96d92a43b0614892609fdaf2b4beacdc1d280
yeoni96 <115720940+yeoni96@users.noreply.github.com>
2022-11-29 22:37:39 +0900

    Add files via upload

--- 221129-first window/src/ColorFrame.java
+++ 221129-first window/src/ColorFrame.java
@@ -9,39 +9,29 @@ import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
-class MyClickEventListener implements ActionListener {
-	
-	@Override
-	public void actionPerformed(ActionEvent e) {
-		JPanel pn1 = new JPanel();
-		int colorNum1 = (int)(Math.random() * 255) + 1;
-		int colorNum2 = (int)(Math.random() * 255) + 1;
-		int colorNum3 = (int)(Math.random() * 255) + 1;
-		pn1.setBackground(new Color(colorNum1, colorNum2, colorNum3));
-	}
-	
-}
 
-
-public class ColorFrame extends JFrame {
-	
+public class ColorFrame extends JFrame implements ActionListener {
+	JPanel pn1 = new JPanel();
 	public ColorFrame() {
 		super("색이 있는 창");
-		
-		
 		// JPanel == 컨테이너 역할을 하는 도화지
-		JPanel pn1 = new JPanel();
-		
 		pn1.setBackground(new Color(0, 0, 0));
 		add(pn1);
 		JButton btn = new JButton("색 바꾸기");
 		pn1.add(btn);
 		Container container = getContentPane();
-		ActionListener listener = new MyClickEventListener();
-		btn.addActionListener(listener);
+		btn.addActionListener(this);
 		setSize(500, 500);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		
+	}
+	
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		int colorNum1 = (int)(Math.random() * 255) + 1;
+		int colorNum2 = (int)(Math.random() * 255) + 1;
+		int colorNum3 = (int)(Math.random() * 255) + 1;
+		pn1.setBackground(new Color(colorNum1, colorNum2, colorNum3));
 	}
 	
 	public void showGUI() {
