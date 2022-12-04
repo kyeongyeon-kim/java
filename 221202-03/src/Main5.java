792773e09da53083dc885da61fc7480d8554e9c7
yeoni96 <115720940+yeoni96@users.noreply.github.com>
2022-12-04 21:48:59 +0900

    Add files via upload

--- 221202-03/src/Main5.java
+++ 221202-03/src/Main5.java
@@ -28,43 +28,6 @@ class QuizFrame extends JFrame {
 	private int sum;
 	private JTextField input;
 	
-	class SubFrame extends JDialog {
-		
-		public SubFrame() {
-			JPanel main = new JPanel();
-			JLabel answerOrWrong = new JLabel("정답입니다.");
-			JButton btnOK = new JButton("확인");
-			main.setLayout(new BoxLayout(main, BoxLayout.Y_AXIS));
-			answerOrWrong.setAlignmentX(CENTER_ALIGNMENT);
-			btnOK.setAlignmentX(CENTER_ALIGNMENT);
-			
-			btnOK.addActionListener(new ActionListener() {
-				
-				@Override
-				public void actionPerformed(ActionEvent e) {
-					num1 = random.nextInt(100);
-					num2 = random.nextInt(100);
-					sum = num1 + num2;
-					quiz.setText("Quiz" + "(" + (count + 1) + ")    " + num1 + " + " + num2 + "=  ?");
-					input.setText("");
-					dispose();
-				}
-			});
-			main.add(Box.createHorizontalStrut(1));
-			main.add(answerOrWrong);
-			main.add(Box.createHorizontalStrut(1));
-			main.add(btnOK);
-			main.add(Box.createHorizontalStrut(1));
-			
-			pack();
-			add(main);
-			setModal(true);
-			setSize(300, 200);
-			setVisible(true);
-			setLocationRelativeTo(null);
-		}
-	}
-	
 	public QuizFrame() {
 		JPanel main = new JPanel();
 		JPanel north = new JPanel();
@@ -72,8 +35,8 @@ class QuizFrame extends JFrame {
 		JPanel south = new JPanel();
 		main.setLayout(new GridLayout(3, 1));
 		
-		num1 = random.nextInt(100);
-		num2 = random.nextInt(100);
+		num1 = random.nextInt(101);
+		num2 = random.nextInt(101);
 		sum = num1 + num2;
 		
 		quiz = new JLabel("Quiz" + "(" + (count + 1) + ")    " + num1 + " + " + num2 + " =  ?");
@@ -90,11 +53,15 @@ class QuizFrame extends JFrame {
 			public void actionPerformed(ActionEvent e) {
 				int answer = Integer.parseInt(input.getText());
 				if (answer == sum) {
-					SubFrame sub = new SubFrame();
+					JOptionPane.showMessageDialog(null, "정답입니다.", "정답", JOptionPane.INFORMATION_MESSAGE);
 					count++;
 					String showScore = String.valueOf(count);
 					score.setText("현재까지 점수 : " + showScore);
+					num1 = random.nextInt(101);
+					num2 = random.nextInt(101);
+					sum = num1 + num2;
 					quiz.setText("Quiz" + "(" + (count + 1) + ")    " + num1 + " + " + num2 + " =  ?");
+					input.setText("");
 				} else {
 					JOptionPane.showMessageDialog(null, "오답입니다.", "오답", JOptionPane.INFORMATION_MESSAGE);
 					count = 0;
