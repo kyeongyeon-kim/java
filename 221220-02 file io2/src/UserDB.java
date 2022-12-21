34a24b5b2320e89c8fc4d654ebf95b09a8b4401a
yeon966 <115720940+yeon966@users.noreply.github.com>
2022-12-21 13:45:29 +0900

    Delete UserDB.java

--- 221220-02 file io2/src/UserDB.java
+++
@@ -1,8 +0,0 @@
-import java.util.ArrayList;
-import java.util.List;
-
-public class UserDB {
-	public static List<String> NAME = new ArrayList<>();
-	public static List<String> PHONE = new ArrayList<>();
-
-}
