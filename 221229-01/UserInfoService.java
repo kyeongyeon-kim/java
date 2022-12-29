8224e38890f0cc5a33fb89345833acbd39668d35
yeon966 <115720940+yeon966@users.noreply.github.com>
2022-12-29 23:02:48 +0900

    Add files via upload

---
+++ 221229-01/UserInfoService.java
@@ -0,0 +1,8 @@
+package ddaa;
+public interface UserInfoService {
+	int INPUT_NOT_VALID = -1;
+	int EMAIL_DUPLICATED = -2;
+
+	int userin(String email, String firstname, String lastname);
+	int userout(int id);
+}
---
+++ 221229-01/UserinfoRepository.java
@@ -0,0 +1,7 @@
+package ddaa;
+public interface UserinfoRepository {
+	// DB입출력의 기능만 구현
+	int countByEmail(String email);
+	Userinfo selectByEmail(String email);
+	int insert(String email, String firstname, String lastname); // pk반환
+}
