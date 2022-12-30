79b875859221c5bf41598485ea28e95f89983b66
yeon966 <115720940+yeon966@users.noreply.github.com>
2022-12-30 17:46:55 +0900

    Delete 221229-01 directory

--- 221229-01/InputNotValidException.java
+++
@@ -1,8 +0,0 @@
-package ddaa;
-// 언체크 예외로 예외 클래스 만들어보기
-public class InputNotValidException extends RuntimeException {
-	public InputNotValidException(String message) {
-		super(message);
-	}
-
-}
--- 221229-01/UserInfoService.java
+++
@@ -1,8 +0,0 @@
-package ddaa;
-public interface UserInfoService {
-	int INPUT_NOT_VALID = -1;
-	int EMAIL_DUPLICATED = -2;
-
-	int userin(String email, String firstname, String lastname);
-	int userout(int id);
-}
