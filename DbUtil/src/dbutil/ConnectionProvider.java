92858f3f714e77aaf8042e76db41e96f7e9c8510
yeon966 <115720940+yeon966@users.noreply.github.com>
2023-01-12 17:06:09 +0900

    Add files via upload

--- DbUtil/src/dbutil/ConnectionProvider.java
+++ DbUtil/src/dbutil/ConnectionProvider.java
@@ -1,25 +1,47 @@
 package dbutil;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.Properties;
 
 public class ConnectionProvider {
 	// 스태틱 블럭 == 클래스로드와 동시에 실행되는 블럭
+	private static final Properties PROPERTIES = new Properties();
+	
 	static {
-		try {
+		InputStream is = null;
+		try {ClassLoader loader = ConnectionProvider.class.getClassLoader();
+			is = loader.getResourceAsStream("local.properties");
+			
+			PROPERTIES.load(is);
+			
+			
+			
 			Class.forName("com.mysql.cj.jdbc.Driver");
 		} catch (ClassNotFoundException e) {
 			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			if (is != null) {
+				try {
+					is.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}	
+			}
 		}
 	}
 
 	public static Connection makeConnection() throws SQLException {
 		Connection conn = null;
 		// 주소에 데이터베이스 USE 사용가능
-		conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/my_db", "root", "root");
+		conn = DriverManager.getConnection(PROPERTIES.getProperty("url"), PROPERTIES.getProperty("username"), PROPERTIES.getProperty("password"));
 		return conn;
 	}
 
